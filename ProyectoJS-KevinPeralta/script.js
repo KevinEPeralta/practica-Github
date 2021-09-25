e5aa6ebd68435df1b5b6f73d9cd26d1e77ab6bac
KevinEPeralta <86889239+KevinEPeralta@users.noreply.github.com>
2021-09-25 20:17:49 -0300

    Add files via upload

--- ProyectoJS-KevinPeralta/script.js
+++ ProyectoJS-KevinPeralta/script.js
@@ -9,12 +9,14 @@ class Cancha{
 }
 
 class Reserva{
+    id;
     dia;
     horario;
     cliente;
     cancha;
 
     constructor(dia,horario,cliente,cancha){
+        this.id = cancha + dia + horario;
         this.dia = dia;
         this.horario = horario;
         this.cliente = cliente;
@@ -25,82 +27,105 @@ class Reserva{
         
         let reservasStorage = JSON.parse(localStorage.getItem("reservas"))
 
-        const reserva = {dia: this.dia, 
-            horario:this.horario,
-            cliente: this.cliente,
-            cancha:this.cancha
-        }
-
         if(reservasStorage){
-            reservasStorage.push(reserva)         
+            reservasStorage.push(this)         
         }else{    
-            reservasStorage=[reserva]        
+            reservasStorage=[this]        
         }
-        
-        document.getElementById(`${reserva.cancha}${reserva.dia}${reserva.horario}`).innerHTML = `<button style="background-color:red">Reservado</button>`        
+          
+        $(`#${this.id}`).html(`<button class="reservado" onclick = "verReserva('${this.id}')">Reservado</button>`)      
         localStorage.setItem("reservas", JSON.stringify(reservasStorage));
+    }
+
+    eliminarReserva = () =>{
+        const reservasStorage = JSON.parse(localStorage.getItem("reservas"));
+        let reservaCancelar;
+        const reservasFiltradas = reservasStorage.filter(reserva => {
+
+            if(reserva.id === this.id){
+                reservaCancelar = reserva
+                return false;
+            }
+            return true;
+        })
+        
+        $(`#${this.id}`).html(`<button class="reservar" onclick = "reservar('${reservaCancelar.cancha}',${reservaCancelar.dia},${reservaCancelar.horario})">Reservar</button>`) 
+        localStorage.setItem("reservas", JSON.stringify(reservasFiltradas));        
     }
 }
 
 //Inicializo cada cancha, con sus horarios de apertura y cierre
 const canchas = {
-    futbol: new Cancha(10,23),
-    tenis: new Cancha(8,17),
-    padel: new Cancha(16,23)
+    Futbol: new Cancha(10,23),
+    Tenis: new Cancha(8,17),
+    Padel: new Cancha(16,23)
 }
  
-if(!localStorage.getItem("dias")){
-     dias = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
-    localStorage.setItem("dias", JSON.stringify(dias))
-}else{
-     dias = JSON.parse(localStorage.getItem("dias"))
-}
- 
+const dias = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
+
 $(() => {
     $("#deportes").change ( evento => {
         const deporte = evento.target.value;
         let elementosDias='';
 
         for (const dia of dias) {
-            elementosDias += `<th>${dia}</th>` 
+            elementosDias += `<th class='bordeTurnos'>${dia}</th>` 
         }
-
-        //document.getElementById("reservas").innerHTML = 
-        $("#reservas").html("<tr><th>Horarios</th>"+elementosDias+"</tr>")
+        $("#datosReserva").fadeOut();
+         
+        $("#reservas").html("<tr><th class='bordeTurnos'>Horarios</th>"+elementosDias+"</tr>")
 
             for (let horario = canchas[deporte].horarioApertura; horario <= canchas[deporte].horarioCierre; horario++) {    
-                const fila = document.createElement("tr");  
-                fila.innerHTML = `<td>${horario}:00</td>`;
+                const fila = $(`<tr><td class="bordeTurnos">${horario}:00</td></tr>`)
 
-            for (let dia = 0; dia < 7; dia++) {
-                let reservasStorage = JSON.parse(localStorage.getItem("reservas"))
-                if (reservasStorage) {                    
-                    const reservado = () =>{
-                        let encontrado;
-                        encontrado = reservasStorage.find(reserva => reserva.dia === dia && reserva.horario === horario && reserva.cancha === deporte)                        
-                        return encontrado;                                                       
-                    }
-                    if (reservado()) {
-                        fila.innerHTML += `<td id='${deporte}${dia}${horario}'><button style="background-color:red">Reservado</button></td>`
+                for (let dia = 0; dia < 7; dia++) {
+                    const reservasStorage = JSON.parse(localStorage.getItem("reservas"))
+                    if (reservasStorage) {             
+                        const reserva = reservasStorage.find(reserva => reserva.id === `${deporte}${dia}${horario}`)                     
+                        if (reserva) {
+                            fila.append(`<td id='${deporte}${dia}${horario}'><button class="reservado" onclick = "verReserva('${deporte}${dia}${horario}')">Reservado</button></td>`)
+                        }else{
+                            fila.append(`<td id='${deporte}${dia}${horario}'><button class="reservar" onclick = "reservar('${deporte}',${dia},${horario})">Reservar</button></td>`)
+                        }                    
                     }else{
-                        fila.innerHTML += `<td id='${deporte}${dia}${horario}'><button onclick = "reservar('${deporte}',${dia},${horario})">Reservar</button></td>`
-                    }                    
-                }else{
-                    fila.innerHTML += `<td id='${deporte}${dia}${horario}'><button onclick = "reservar('${deporte}',${dia},${horario})">Reservar</button></td>`
-                }
-            }     
-            $("#reservas").append(fila);           
+                        fila.append(`<td id='${deporte}${dia}${horario}'><button class="reservar" onclick = "reservar('${deporte}',${dia},${horario})">Reservar</button></td>`)
+                    }
+                }     
+                $("#reservas").append(fila);           
         }            
     })    
 })  
 
 const reservar = (deporte, dia, horario) =>{
-    
+    $("#datosReserva").fadeOut()
     const cliente = $("#cliente").val();
     if (cliente ===""){
-        alert("No ingresó ningún nombre para reservar!, llene la casilla indicada como Nombre de reserva."   )
-        exit;
+        $("#alerta").fadeIn(2000)
+        $("#alerta").fadeOut(3000)
     }else{
         new Reserva(dia,horario,cliente,deporte).reservar();
     }
 }
+
+const verReserva = id => {
+    const reservasStorage = JSON.parse(localStorage.getItem("reservas"));
+    const reserva = reservasStorage.find(reserva => reserva.id === id)  
+
+    $(`#datosReserva`).html(
+        `<div class="reserva">
+        <h3>Reserva</h3>
+        <p>Cliente:${reserva.cliente}</p>
+        <p>Cancha:${reserva.cancha}</p>
+        <p>Día:${dias[reserva.dia]} Horario:${reserva.horario}:00</p>
+        <button class="reservado"onclick ="eliminarReserva('${reserva.id}')">Cancelar reserva</button>
+        </div>
+        `)
+        $(`#datosReserva`).fadeIn()
+}
+
+const eliminarReserva = id =>{    
+    const reserva = new Reserva()
+    reserva.id = id
+    reserva.eliminarReserva()
+}
+
